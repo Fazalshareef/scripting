cad74fc6ef8c722ead33b0da30c2e4db67730e88
fazal <mohammedfazalshareef@gmail.com>
2025-07-09 21:17:51 +0530

    added arrays concept

---
+++ 08.Conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER gt 50 ]
+then
+   echo "The given number $NUMBER is greater than 50"
+else 
+    echo "The given number $NUMBER is less than or equal to 50"
+fi
