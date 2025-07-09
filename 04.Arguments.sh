0f7a17b125c46522a124dfddc8b049eb835fcba3
fazal <mohammedfazalshareef@gmail.com>
2025-07-09 13:17:08 +0530

    arguments added

---
+++ 04.Arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2  
+
+echo "$PERSON1 : Hello $PERSON2 how are you."
+echo "$PERSON2 : I am fine $PERSON1, how are you?"
+echo "$PERSON1 : I am fine too, thanks for asking $PERSON2."        
+echo "$PERSON2 : You are welcome $PERSON1."
