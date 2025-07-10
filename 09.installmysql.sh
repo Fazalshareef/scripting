0ea009ee8b9877daa205061f2c3e4a665bd17c67
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 10:30:00 +0530

    added arrays concept

--- 09.installmysql.sh
+++ 09.installmysql.sh
@@ -2,13 +2,23 @@
 
 ID=$(id -u)
 
+INSTALL_SOFTWARE
+
 if [ $ID -ne 0 ]
 
 then 
     echo "Error :: Please access with root user"
-    exit 1
+    exit 1 # Other than zero (zero means it has been exeuted sucessfully)
 else 
   echo "Okay you are a root user "
 fi
 
-yum install nginx -y
+yum install mysql -y
+
+if [ $? -ne 0]
+ 
+then
+    echo "Failed to Install"
+else 
+    echo "Installed Sucessfully"
+fi        
