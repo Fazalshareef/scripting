de8930a009761ea1ee08c70819791d0e018fd33d
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 10:09:43 +0530

    added arrays concept

---
+++ 09.installmysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+
+then 
+    echo "Error :: Please access with root user"
+    exit 1
+else 
+  echo "Okay you are a root user "
+fi
+
+yum install mysql -y
