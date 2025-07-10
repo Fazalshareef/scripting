25bfb8fe3ff0706666e723cced2579a07388602f
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 10:55:38 +0530

    added arrays concept

---
+++ 10.installpackages.sh
@@ -0,0 +1,31 @@
+
+
+ID=$(id -u)
+
+if [ $ID -eq 0 ]
+then 
+ echo "You are root user the script will start executing"
+else 
+ echo "Please run the script with root acess or by SUDO"
+ exit 1
+fi
+
+yum install htosp -y
+
+if [ $? -eq 0 ]
+then 
+  echo "Installed Sucessfully"
+else
+  echo "Failed to install, please check the command"
+  exit 1
+fi
+
+yum install wget -y
+
+if  [ $? -eq 0 ]
+then 
+    echo "Installed Sucessfully"
+else 
+    echo "Failed to install, please check the command"
+    exit 1
+fi        
