0aebf554e03a10407d221ff64e456ae5a33f76d0
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 19:48:22 +0530

    added arrays concept

---
+++ 15. Package_Install_By_Loops.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP &>> $LOGFILE"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N"
+    else
+        echo -e "$2...$G SUCESS $N"    
+}
+
+if [ $ID -eq 0 ]
+then 
+    echo -e "You are the root User. $G Script Started executing $N"
+else
+    echo -e "Please run the script with $R Root Acess 'sudo' $N"    
+fi
+
+
+for package in $0
+do
+    yum installed $package &>> $LOGFILE
+        if [ $? -eq 0 ]
+        then
+            echo -e "Package already exists... $Y Skipping $N"
+        else
+            yum install $package -y &>> $LOGFILE
+            VALIDATE $? "Installation of $package"   
+done
