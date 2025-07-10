df8e3c5f976502023a3537023016b7deec12bd26
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 12:48:25 +0530

    added arrays concept

--- 12.logs.sh
+++ 12.logs.sh
@@ -1,34 +1,27 @@
-
+#!/bin/bash
 
 ID=$(id -u)
-
 TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$(basename $0)-$TIMESTAMP.log"
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-if [ $ID -eq 0 ]
-then 
-    echo "You are a root User"
+if [ $ID -eq 0 ]; then 
+    echo "You are a root user"
 else
-    echo "Please run the script with root acess"
+    echo "Please run the script with root access"
     exit 1
 fi
 
-VALIDATE (){
-   if [ $1 -eq 0 ]
-   then 
-    echo "$2...Sucess"
-   else
-     echo "Failed to Insatll $2. Please check the command again"
-     exit 1
+validate() {
+    if [ $1 -eq 0 ]; then 
+        echo "$2...Success"
+    else
+        echo "Failed to install $2. Please check the command again"
+        exit 1
     fi   
 }
 
 yum install cowsay -y &>> $LOGFILE
+validate $? "Installing cowsay"
 
-VALIDATE $? "Installing cowsay"
-
-yum install nano -y. &>> $LOGFILE
-
-VALIDATE $? "Installing nano"
-
+yum install nano -y &>> $LOGFILE
+validate $? "Installing nano"
