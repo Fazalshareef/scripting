64235d7aa2a2641f0dc74fcc88296fbe1e84d9c1
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 12:43:22 +0530

    added arrays concept

---
+++ 12.logs.sh
@@ -0,0 +1,34 @@
+
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+if [ $ID -eq 0 ]
+then 
+    echo "You are a root User"
+else
+    echo "Please run the script with root acess"
+    exit 1
+fi
+
+VALIDATE (){
+   if [ $1 -eq 0 ]
+   then 
+    echo "$2...Sucess"
+   else
+     echo "Failed to Insatll $2. Please check the command again"
+     exit 1
+    fi   
+}
+
+yum install cowsay -y &>> LOGFILE
+
+VALIDATE $? "Installing cowsay"
+
+yum install nano -y. &>> LOGFILE
+
+VALIDATE $? "Installing nano"
+
