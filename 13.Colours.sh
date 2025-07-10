42f1fde5fac1ac532c99b4b40f3c92f75e5aff25
fazal <mohammedfazalshareef@gmail.com>
2025-07-10 12:56:48 +0530

    added arrays concept

---
+++ 13.Colours.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$(basename $0)-$TIMESTAMP.log"
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+if [ $ID -eq 0 ]; then 
+    echo "You are a root user"
+else
+    echo "$R Please run the script with root access $N"
+    exit 1
+fi
+
+validate() {
+    if [ $1 -eq 0 ]; then 
+        echo "$2...$G Success $N" 
+    else
+        echo "$R Failed to install $N $2. Please check the command again"
+        exit 1
+    fi   
+}
+
+yum install cowsfay -y &>> $LOGFILE
+validate $? "Installing cowsay"
+
+yum install nano -y &>> $LOGFILE
+validate $? "Installing nano"
